da2e2c2efbe5c620ee6bc6b73a8a86100edd6beb
youtissoum <youtissoum@outlook.fr>
2022-04-29 17:36:25 +0200

    project made

---
+++ src/main/java/me/youtissoum/thephonemod/ThePhoneMod.java
@@ -0,0 +1,10 @@
+package me.youtissoum.thephonemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ThePhoneMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/youtissoum/thephonemod/client/ThePhoneModClient.java
@@ -0,0 +1,13 @@
+package me.youtissoum.thephonemod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ThePhoneModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
